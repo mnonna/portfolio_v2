5014ccec5bf3b151aaf8e32561ea72993ca65163
mnonna <marcinnonna026@gmail.com>
2020-09-02 17:02:06 +0200

    Optimized webpack bundle size. Optimized image files size

--- src/main.js
+++ src/main.js
@@ -1,12 +1,19 @@
 import Vue from "vue";
 import App from "./App.vue";
-import BootstrapVue from 'bootstrap-vue'
+import { SpinnerPlugin } from "bootstrap-vue";
+import { BNavbarBrand, BNavbar, BRow, BCol, BContainer } from "bootstrap-vue";
 
 Vue.config.productionTip = false;
-Vue.use(BootstrapVue);
 
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
+Vue.use(SpinnerPlugin);
+Vue.component("b-navbar-brand", BNavbarBrand);
+Vue.component("b-navbar", BNavbar);
+Vue.component("b-container", BContainer);
+Vue.component("b-row", BRow);
+Vue.component("b-col", BCol);
+
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
 
 new Vue({
   render: h => h(App)
